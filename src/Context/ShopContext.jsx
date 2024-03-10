6e5db42fca2a054b75244e4eaf1fe9c93ee6bbdb
Anmol Garg <128157063+AnmollGarg@users.noreply.github.com>
2024-03-10 14:46:55 +0530

    Before Responsiveness Code

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import all_product from "../Comp/Assets/all_product";
 import { createContext } from "react";
+import CartItems from "../Comp/CartItems/CartItems";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
-    for (let index = 0; index < all_product.length + 1; index++) {
+    for (let index = 0; index < all_product.length; index++) {
         cart[index] = 0;
     }
     return cart;
@@ -15,19 +16,36 @@ const getDefaultCart = () => {
 const ShopContextProvider = (props) => {
     const [cartItem, setCartItem] = useState(getDefaultCart());
 
-
     const addToCart = (itemId) => {
         setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-        console.log(cartItem);
     };
 
     const removeFromCart = (itemId) => {
         setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     };
 
-    // console.log(cartItem);
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItem) {
+            if (cartItem[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item));
+                totalAmount += itemInfo.new_price * cartItem[item];
+            }
+        }
+        return totalAmount;
+    }
 
-    const contextValue = { all_product, cartItem, addToCart, removeFromCart };
+    const getTotalCartItems = () => {
+        let totalItems = 0;
+        for (const item in cartItem) {
+            if (cartItem[item] > 0) {
+                totalItems += cartItem[item];
+            }
+        }
+        return totalItems;
+    }
+
+    const contextValue = { getTotalCartItems, getTotalCartAmount, all_product, cartItem, addToCart, removeFromCart };
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
